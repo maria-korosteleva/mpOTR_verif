03ef26b8f0890199101132b0f8e084571f643db7
Maria Korosteleva <mariak@seclab.cs.msu.su>
2014-10-10 13:38:16 +0400

    first steps

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
-date
-#!/bin/bash
+#!/bin/sh
 /home/mary/maude/maude.linux64 <<EOF
 load /home/mary/maude/full-maude26.maude
 load /home/mary/maude-npa-v2_0/maude-npa.maude
-load /home/mary/mpOTR_communic.maude
+load /home/mary/mpOTR_verif/mpOTR_communic.maude
 
 EOF
-date
