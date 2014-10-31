f48971861a5614ac29e7e856ded1b1252e53d61e
Maria Korosteleva <mariak@seclab.cs.msu.su>
2014-10-31 16:16:43 +0400

    Cleaning old versions of run

--- auth/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-/home/mary/maude/maude.linux64 <<EOF
-load /home/mary/maude/full-maude26.maude
-load /home/mary/maude-npa-v2_0/maude-npa.maude
-load /home/mary/mpOTR_verif/auth/mpOTR_auth.maude
-
-EOF
--- communication/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-/home/mary/maude/maude.linux64 <<EOF
-load /home/mary/maude/full-maude26.maude
-load /home/mary/maude-npa-v2_0/maude-npa.maude
-load /home/mary/mpOTR_verif/communic/mpOTR_communic.maude
-
-EOF
