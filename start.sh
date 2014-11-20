0b0a5460d7517d9adf76b875e987befe1a9bbe4b
Maria Korosteleva <mariak@seclab.cs.msu.su>
2014-11-20 19:40:30 +0400

    working with timw

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # the script assumes one parameter -- $1 is a module name to test 
-/home/mary/maude/maude.linux64 <<EOF
+/home/mary/maude/maude.linux64 > /home/mary/mpOTR_verif/$1/mpOTR_$1_grammars.txt <<EOF
 load /home/mary/maude/full-maude26.maude
 load /home/mary/maude-npa-v2_0/maude-npa.maude
 load /home/mary/mpOTR_verif/$1/mpOTR_$1.maude
-
+red genGrammars .
 EOF
